cd36b23087eb655dcb70016056b601644543156f
maureenrnx <maureen.renaux@gmail.com>
2023-01-20 11:05:20 +0100

    add : test

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -15,6 +15,40 @@ describe("validations tests suites - isEmpty", () => {
         const result = isEmpty("Label");
         expect(result).toBe(false);
     });
+
+    test("should return true as the label is contain 8 numbers", () =>{
+        const result = isEmpty("Labellll");
+        expect(result).toBe(true);
+    });
+
+    test("should return false as the label is contain 8 numbers", () =>{
+        const result = isEmpty("Labellll");
+        expect(result).toBe(false);
+    });
+
+    test("should return true if the label contain 1 special char" , () => {
+        const result = isEmpty("Label-l");
+        expect(result).toBe(true);
+    });
+
+    test("should return false if the label contain 1 special char" , () => {
+        const result = isEmpty("Label-l");
+        expect(result).toBe(false);
+    });
+
+    test("should return true if the label contain 1 number" , () => {
+        const result = isEmpty("Label1");
+        expect(result).toBe(true);
+    });
+
+
+    test("should return false if the label contain 1 number" , () => {
+        const result = isEmpty("Label1");
+        expect(result).toBe(false);
+    });
+
+
+
 });
 
 // TODO: Create tests suite for validation function
--- src/services/validations.js
+++ src/services/validations.js
@@ -2,4 +2,11 @@
 
 const isEmpty = (label) => !label || label.length === 0;
 
+if (label === undefined) {
+    return false;
+  }
+  // eslint-disable-next-line no-useless-escape
+  const regex = /[^a-zA-Z0-8]/;
+  if (label.match(regex)) return true;
+
 exports.isEmpty = isEmpty;
